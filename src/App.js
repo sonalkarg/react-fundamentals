e6dbcd14ddbf0001366f0f3489198fc301f8434c
gautam.sonalkar1 <gautam.sonalkar1@kantar.com>
2022-06-27 23:56:23 +0530

    Split screen component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { SplitScreen } from "./SplitScreen";
+
+const LeftComp = ({ name }) => {
+  return <h1 style={{ backgroundColor: "green" }}>{name}</h1>;
+};
+
+const RightComp = ({ message }) => {
+  return <h1 style={{ backgroundColor: "red" }}>{message}</h1>;
+};
+
 function App() {
   return (
     <>
-      <h1>Base Create React App</h1>
+      <SplitScreen leftWeight={1} rightWeight={3}>
+        <LeftComp name={"Gautam"} />
+        <RightComp message={"I like React"} />
+      </SplitScreen>
     </>
   );
 }
